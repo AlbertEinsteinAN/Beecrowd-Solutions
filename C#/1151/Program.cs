e0f339f1acda8f7304e5eccd3304d3156d5c9c81
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-06-03 06:42:28 -0300

    1151

--- C#/1151/Program.cs
+++ C#/1151/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1151
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int a = 0, b = 1, c = 0;
+
+            if (n == 0 || n == 1)
+            {
+                Console.Write(a);
+                return;
+            }
+
+            Console.Write(a);
+
+            for (int i = 2; i < n; i++)
+            {
+                a = b;
+                b = c;
+                c = a + b;
+                
+                Console.Write($" {c}");
+            }
+
+            Console.WriteLine($" {c + b}");
+        }
+    }
+}
