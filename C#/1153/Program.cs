b2722995fb467459eb4e54b7fd7c8276f8ca7d0e
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-06-04 07:16:54 -0300

    1153

---
+++ C#/1153/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1153
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 1;
+
+            if (n <= 0)
+            {
+                return;
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                sum = i * sum;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
