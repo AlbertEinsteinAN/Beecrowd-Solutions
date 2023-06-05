6cc4794fa9a572b42d6b4ddf0d8ace934659daae
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-06-05 04:17:52 -0300

    1154

---
+++ C#/1154/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Beecrowd
+{
+    class Problem1154
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0, result = 0, n = 0, times = 0;
+            CultureInfo culture = new CultureInfo("en-US");
+
+            while (n >= 0)
+            {  
+                times++;
+                sum += n;
+                n = double.Parse(Console.ReadLine(), culture);
+            }
+
+            result = sum / (times - 1);
+
+            Console.WriteLine(result.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
