a8336cd1c06c9834e2f5c29f6101e3504c9b1430
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-06-14 04:22:55 -0300

    1177

---
+++ C#/1177/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1177
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            int[] values = new int[1000];
+
+            for (int i = 0; i < 1000; i++)
+            {
+                values[i] = i % t;
+            }
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine($"N[{i}] = {values[i]}");
+            }
+        }
+    }
+}
