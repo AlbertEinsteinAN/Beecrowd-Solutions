079d700f0595ea315054b70876562ea8b38671e0
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-06-15 16:32:12 -0300

    1180

---
+++ C#/1180/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1180
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()), position = 0, smaller = 0;
+            int[] x = new int[n];
+            string[] input = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < n; i++)
+            {
+                x[i] = int.Parse(input[i]);
+
+                if (i == 0)
+                {
+                    int firstInput = x[i];
+                    smaller = firstInput;
+                }
+
+                if (x[i] < smaller)
+                {
+                    position = i;
+                    smaller = x[i];
+                }
+            }
+
+            Console.WriteLine($"Menor valor: {smaller}");
+            Console.WriteLine($"Posicao: {position}");
+        }
+    }
+}
