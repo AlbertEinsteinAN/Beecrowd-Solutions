c0d4edd405577da8be74baeeb4fc7702afd3d13e
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-06-17 06:58:30 -0300

    1864

---
+++ C#/1864/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1864
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "LIFE IS NOT A PROBLEM TO BE SOLVED";
+            int n = int.Parse(Console.ReadLine());
+
+            if (n >= 1 && n <= phrase.Length)
+            {
+                string output = phrase.Substring(0, n);
+                Console.WriteLine(output);
+            }
+
+            else
+            {
+                Console.WriteLine("LIFE IS NOT A PROBLEM TO BE SOLVED");
+            }
+        }
+    }
+}
