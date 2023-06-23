0eed09ab5054ac03106851d659e6a211e55c6e23
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-06-23 04:35:02 -0300

    1071

--- C#/1071/Program.cs
+++ C#/1071/Program.cs
@@ -8,44 +8,28 @@ namespace Beecrowd
         {
             int x = int.Parse(Console.ReadLine());
             int y = int.Parse(Console.ReadLine());
-            int x2 = 0, y2 = 0, count = 0;
+            int sum = 0, min = 0, max = 0;
 
-            if (x > y)
+            if (x < y)
             {
-                while (x > y2)
-                {
-                    if (y != y2)
-                    {
-                        if (y2 % 2 != 0)
-                        {
-                            count += y2;
-                        }
-                    }                    
-                    y2++;                    
-                }
-
-                Console.WriteLine(count);
+                min = x;
+                max = y;
             }
-
-            else if (y > x)
-            {
-                while (y > x2)
-                {
-                    if (x2 % 2 != 0)
-                    {
-                        count += x2;
-
-                    }
-                    x2++;
-                }
-
-                Console.WriteLine(count);
-            }
-
             else
             {
-                Console.WriteLine("0");
+                min = y;
+                max = x;     
             }
+
+            for (int i = (min + 1); i < max; ++i)
+            {
+                if (i % 2 != 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
