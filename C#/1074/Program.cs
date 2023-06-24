834cc980fe99a4cebcefad9c9c4ef328c59418d2
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-06-24 08:54:43 -0300

    1074

---
+++ C#/1074/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1074
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] values = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                values[i] = int.Parse(Console.ReadLine().Split(' ')[0]);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if ((values[i] < 0) && (values[i] %= 2) == 0)
+                {
+                    Console.WriteLine("EVEN NEGATIVE");
+                }
+                else if ((values[i] < 0) && (values[i] %= 2) != 0)
+                {
+                    Console.WriteLine("ODD NEGATIVE");
+                }
+                else if ((values[i] > 0) && (values[i] %= 2) == 0)
+                {
+                    Console.WriteLine("EVEN POSITIVE");
+                }
+                else if ((values[i] > 0) && (values[i] %= 2) != 0)
+                {
+                    Console.WriteLine("ODD POSITIVE");
+                }
+                else
+                {
+                    Console.WriteLine("NULL");
+                }
+            }
+        }
+    }
+}
