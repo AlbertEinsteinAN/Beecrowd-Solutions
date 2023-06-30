b424a949c9c83090efcfb045e9dd257dc60a01ab
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-06-30 04:17:28 -0300

    1097

---
+++ C#/1097/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1097
+    {
+        static void Main(string[] args)
+        {
+            int n = 1, j1 = 7, j2 = 6, j3 = 5;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"I={n} J={j1}");
+                Console.WriteLine($"I={n} J={j2}");
+                Console.WriteLine($"I={n} J={j3}");
+
+                n += 2;
+                j1 += 2;
+                j2 += 2;
+                j3 += 2;
+            }
+        }
+    }
+}
