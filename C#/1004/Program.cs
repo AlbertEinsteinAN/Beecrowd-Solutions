c9a9ac5e2d401f7489944632ab9f5f2f446fdf66
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-07-05 04:50:52 -0300

    1004 - Simple Product

---
+++ C#/1004/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1004
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine($"PROD = {a * b}");
+        }
+    }
+}
