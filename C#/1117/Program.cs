f1a58297a11946902c5ae255ec075fe152bfe913
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-07-06 00:33:44 -0300

    1117 - Score Validation

---
+++ C#/1117/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Beecrowd
+{
+    class Problem1117
+    {
+        static void Main(string[] args)
+        {
+            double a = -1, b = -1;
+            CultureInfo culture = new CultureInfo("en-US");
+
+            while (a < 0 || a > 10)
+            {
+                a = double.Parse(Console.ReadLine());
+                if (a < 0 || a > 10)
+                    Console.WriteLine("nota invalida");
+            }
+
+            while (b < 0 || b > 10)
+            {
+                b = double.Parse(Console.ReadLine());
+                if (b < 0 || b > 10)
+                    Console.WriteLine("nota invalida");
+            }            
+            Console.WriteLine($"media = {(a + b) / 2}");
+        }
+    }
+}
