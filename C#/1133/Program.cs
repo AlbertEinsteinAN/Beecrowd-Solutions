09e43bd9bac4a62a4525cdbf3fee76b20aea56e9
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-07-09 07:46:12 -0300

    1133 - Rest of a Division

---
+++ C#/1133/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1133
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int aux = 0;
+
+            if (x > y)
+            {
+                aux = y;
+                y = x;
+                x = aux;
+            }
+
+            for (int i = (x + 1); i < y; i++)
+            {                
+                if(i % 5 == 2 || i % 5 == 3)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
