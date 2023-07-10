93f09629c7be83ca1cf0cd35277870185e524322
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-07-10 10:08:10 -0300

    1009 - Salary with Bonus

---
+++ C#/1009/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Beecrowd
+{
+    class Problem1009
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double salary = double.Parse(Console.ReadLine());
+            double sale = double.Parse(Console.ReadLine());
+            double total = salary + 0.15 * sale;
+            CultureInfo culture = new CultureInfo("en-US");
+
+            Console.WriteLine($"TOTAL = R$ {total.ToString("F2", culture)}");
+        }
+    }
+}
