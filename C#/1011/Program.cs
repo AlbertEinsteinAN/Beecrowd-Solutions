9e65ee46f335f44853286dd2b00c034dec617998
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-07-12 04:03:36 -0300

    1011 - Sphere

---
+++ C#/1011/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Beecrowd
+{
+    class Problem1011
+    {
+        static void Main(string[] args)
+        {
+            double x = int.Parse(Console.ReadLine());
+            double volumeSphere = 4 * 3.14159 * x * x * x / 3;
+            CultureInfo culture = new CultureInfo("en-US");
+
+            Console.WriteLine($"VOLUME = {volumeSphere.ToString("F3", culture)}");
+        }
+    }
+}
