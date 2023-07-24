b09f040e25add719e51a390ebe7136d1fe124532
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-07-24 19:31:26 -0300

    1021 - Banknotes and Coins

---
+++ Problems/1021/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Beecrowd
+{
+    class Problem1021
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo culture = new CultureInfo("en-US");
+            double input = double.Parse(Console.ReadLine()) * 100;
+
+            double[] bankNotes = {10000, 5000, 2000, 1000, 500, 200, 100, 50, 25, 10, 5, 1};
+
+            Console.WriteLine("NOTAS:");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine($"{(int)(input / bankNotes[i])} nota(s) de R$ {bankNotes[i] / 100}.00");
+                input %= bankNotes[i];
+            }
+
+            Console.WriteLine("MOEDAS:");
+
+            for (int i = 6; i < 12; i++)
+            {
+                Console.WriteLine($"{(int)(input / bankNotes[i])} moeda(s) de R$ {(bankNotes[i] / 100).ToString("F2", culture)}");
+                input %= bankNotes[i];
+            }
+        }
+    }
+}
