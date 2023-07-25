318c09e5e13e6c5316d74fb2aa4c1f584a6cb0f0
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-07-25 04:17:21 -0300

    1035 - Selection Test 1

---
+++ Problems/1035/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1035
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+            int a = int.Parse(inputs[0]); 
+            int b = int.Parse(inputs[1]);
+            int c = int.Parse(inputs[2]);
+            int d = int.Parse(inputs[3]);
+
+            if (b > c && d > a && c + d > a + b && c >= 0 && d >= 0 && a % 2 == 0)
+                Console.WriteLine("Valores aceitos");
+            else
+                Console.WriteLine("Valores nao aceitos");
+        }
+    }
+}
