2581824a2e477baefa89f2f0d4a390988425edad
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-07-26 03:27:10 -0300

    1184 - Below the Main Diagonal

---
+++ Problems/1184/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Beecrowd
+{
+    class Problem1184
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo culture = new CultureInfo("en-US");
+            double[,] array = new double[12, 12];
+            double sum = 0;
+            char operation = char.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 12; i++)
+                for (int j = 0; j < 12; j++)
+                    array[i, j] = double.Parse(Console.ReadLine());
+
+            for (int i = 1; i < 12; i++)
+                for (int j = 0; j < i; j++)
+                    sum += array[i, j];
+            
+            if (operation == 'S')
+                Console.WriteLine(sum.ToString("F1", culture));
+            else
+                Console.WriteLine((sum / 66).ToString("F1", culture));
+            // The number 66 comes from: ∑[i = 1, 12] ∑[j = 0, i - 1] A(i, j)
+        }
+    }
+}
