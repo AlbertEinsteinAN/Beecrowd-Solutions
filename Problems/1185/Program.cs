140b4553aa7040ae5d2038e1fd8a56e20a5ab85a
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-07-27 04:47:05 -0300

    1185 - Above the Secundary Diagonal

---
+++ Problems/1185/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Beecrowd
+{
+    class Problem1185
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo culture = new CultureInfo("en-US");
+            double[,] array = new double[12, 12];
+            double sum = 0;
+            char operation = char.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 12; i++)
+                for (int j = 0; j < 12; j++)
+                {
+                    array[i, j] = double.Parse(Console.ReadLine());
+
+                    if (j < 11 - i)
+                        sum += array[i, j];
+                }
+
+            if (operation == 'S')
+                Console.WriteLine(sum.ToString("F1", culture));
+            else
+                Console.WriteLine((sum / 66).ToString("F1", culture));
+        }
+    }
+}
