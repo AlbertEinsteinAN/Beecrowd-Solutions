e5f143653909a3b230c7ca01d5f180d6140f9ba5
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-07-28 09:16:04 -0300

    1038 - Snack

---
+++ Problems/1038/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Beecrowd
+{
+    class Problem1038
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo culture = new CultureInfo("en-US");
+            string[] input = Console.ReadLine().Split(' ');
+            double[] price = {4.00, 4.50, 5.00, 2.00, 1.50};
+            int code = int.Parse(input[0]);
+            double quantity = double.Parse(input[1]);
+            double total = price[code - 1] * quantity;
+
+            Console.WriteLine($"Total: R$ {total.ToString("F2", culture)}");
+        }
+    }
+}
