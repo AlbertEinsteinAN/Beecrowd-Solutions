218080f9c1daff23eac7f5cb0a05c180444fa277
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-07-31 09:21:58 -0300

    1042 - Simple Sort

---
+++ VS-files/1042/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1042
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int[] value = {int.Parse(input[0]), int.Parse(input[1]), int.Parse(input[2])};
+
+            Array.Sort(value);
+            Console.WriteLine($"{value[0]}\n{value[1]}\n{value[2]}\n");  
+            Console.WriteLine($"{input[0]}\n{input[1]}\n{input[2]}");
+        }
+    }
+}
