47208402586e46c2600ef18d9de2b578e964c737
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-08-02 20:58:09 -0300

    1043 - Triangle

---
+++ VS-files/1043/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1043
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            double a = double.Parse(input[0]);
+            double b = double.Parse(input[1]);
+            double c = double.Parse(input[2]);
+
+            if (a + b > c && a + c > b && b + c > a)
+            {
+                double perimeter = a + b + c;
+                Console.WriteLine($"Perimetro = {perimeter:F1}");
+            }
+            else
+            {
+                double area = ((a + b) * c) / 2.0;
+                Console.WriteLine($"Area = {area:F1}");
+            }
+        }
+    }
+}
