a2a18a280c82e097e9b7772e681cb4bf198e9085
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-08-03 20:11:32 -0300

    1478 - Square Matrix II

---
+++ VS-files/1478/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1478
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            while (int.TryParse(Console.ReadLine(), out n) && n > 0)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    for (int j = 1; j <= n; j++)
+                    {
+                        if (j > 1) Console.Write(" ");
+                        Console.Write($"{Math.Abs(i - j) + 1, 3}");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
