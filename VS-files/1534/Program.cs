92b4c2eb58b56df9cf2d3ebef8e2700f035f4f47
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-08-04 07:45:33 -0300

    1534 - Array 123

---
+++ VS-files/1534/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program1534
+{
+    static void Main(string[] args)
+    {
+        short tamanho, linha, coluna;
+
+        while (short.TryParse(Console.ReadLine(), out tamanho))
+        {
+            short[,] matriz = new short[tamanho, tamanho];
+
+            for (linha = 0; linha < tamanho; linha++)
+            {
+                for (coluna = 0; coluna < tamanho; coluna++)
+                {
+                    if (linha == coluna)
+                        matriz[linha, coluna] = 1;
+                    if (linha == tamanho - coluna - 1)
+                        matriz[linha, coluna] = 2;
+                    if (linha != coluna && linha != tamanho - coluna - 1)
+                        matriz[linha, coluna] = 3;
+                }
+            }
+
+            for (linha = 0; linha < tamanho; linha++)
+            {
+                for (coluna = 0; coluna < tamanho; coluna++)
+                {
+                    Console.Write(matriz[linha, coluna]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
