c7c25685b6938f1d52c068137a1069cb3fe6007c
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-08-03 21:00:01 -0300

    1044 - Multiples

---
+++ VS-files/1044/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1044
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+
+            if (b % a == 0 || a % b == 0)
+                Console.WriteLine("Sao Multiplos");
+            else
+                Console.WriteLine("Nao sao Multiplos");
+        }
+    }
+}
