16385841825acb688b1d6e8a21f3db22f107bf02
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-08-05 10:36:34 -0300

    1541 - Building Houses

---
+++ VS-files/1541/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1541
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, e, f;
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split();
+                
+                a = int.Parse(input[0]);
+
+                if (a == 0)
+                    break;
+                else
+                {
+                    b = int.Parse(input[1]);
+                    c = int.Parse(input[2]);
+
+                    d = a * b;
+                    e = (d * 100) / c;
+                    f = (int)Math.Sqrt(e);
+                    Console.WriteLine(f);
+                }
+            }
+        }
+    }
+}
