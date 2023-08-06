154eaefba646d35bbf6e1d675b922d3992e755d5
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-08-06 08:42:52 -0300

    1557 - Square Matrix III

---
+++ VS-files/1557/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int order;
+
+        while ((order = int.Parse(Console.ReadLine())) != 0)
+        {
+            int[,] array = GeneratePowerArray(order);
+            PrintFormattedArray(array);
+            Console.WriteLine();
+        }
+    }
+
+    static int[,] GeneratePowerArray(int order)
+    {
+        int[,] array = new int[order, order];
+
+        for (int row = 0; row < order; row++)
+        {
+            for (int col = 0; col < order; col++)
+            {
+                array[row, col] = (int)Math.Pow(2, row + col);
+            }
+        }
+
+        return array;
+    }
+
+    static void PrintFormattedArray(int[,] array)
+    {
+        int maxDigits = GetMaxDigits(array);
+
+        for (int row = 0; row < array.GetLength(0); row++)
+        {
+            for (int col = 0; col < array.GetLength(1); col++)
+            {
+                string formattedNumber = array[row, col].ToString().PadLeft(maxDigits);
+                string separator = col == array.GetLength(1) - 1 ? "" : " ";
+                Console.Write(formattedNumber + separator);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static int GetMaxDigits(int[,] array)
+    {
+        int maxDigits = 0;
+
+        foreach (int num in array)
+        {
+            int digits = (int)Math.Log10(num) + 1;
+            if (digits > maxDigits)
+            {
+                maxDigits = digits;
+            }
+        }
+
+        return maxDigits;
+    }
+}
