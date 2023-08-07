4ac701c515f65979c73a57e7de84f09d6270dad3
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-08-07 06:14:00 -0300

    1564 - Brazil World Cup

---
+++ VS-files/1564/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1564
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            while (int.TryParse(Console.ReadLine(), out n))
+            {
+                if (n != 0)
+                    Console.WriteLine("vai ter duas!");
+                else
+                    Console.WriteLine("vai ter copa!");
+            }
+        }
+    }
+}
