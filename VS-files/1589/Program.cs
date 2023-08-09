e0f12dd019aa4c1b216847c763c1bba938750dd5
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-08-09 06:16:36 -0300

    1589 - Bob Conduit

---
+++ VS-files/1589/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1589
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                int r1 = int.Parse(input[0]);
+                int r2 = int.Parse(input[1]);
+
+                Console.WriteLine(r1 + r2);
+            }
+        }
+    }
+}
