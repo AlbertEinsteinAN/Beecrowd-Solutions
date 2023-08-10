a45c505e56edf9c9dcbc421bf23867c7da457ef7
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-08-10 04:27:06 -0300

    1789 - The Race of Slugs

---
+++ VS-files/1789/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1789
+    {
+        static void Main(string[] args)
+        {
+            string line;
+
+            while ((line = Console.ReadLine()) != null)
+            {
+                int l = int.Parse(line), bigger = 0;
+                int [] values = new int[l];
+                string[] input = Console.ReadLine().Split(' ');
+
+                for (int i = 0; i < l; i++)
+                {
+                    values[i] = int.Parse(input[i]);
+
+                    if (values[i] > bigger)
+                        bigger = values[i];
+                }
+                
+                if (bigger < 10)
+                    Console.WriteLine("1");
+                else if (bigger >= 10 && bigger < 20)
+                    Console.WriteLine("2");
+                else
+                    Console.WriteLine("3");
+            }
+        }
+    }
+}
