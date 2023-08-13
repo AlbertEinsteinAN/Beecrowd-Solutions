d50a34c8fda11eb18f6d17e2220fdd9eeaec90de
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-08-13 07:55:51 -0300

    1837 - Preface

---
+++ VS-files/1837/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1837
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+            int q, r;
+
+            if (a < 0)
+            {
+                int x = Math.Abs(b);
+                for (r = 0; r < x; r++)
+                {
+                    int y = a - r;
+                    if (y % b == 0)
+                    {
+                        q = y / b;
+                        Console.WriteLine($"{q} {r}");
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                q = a / b;
+                r = a % b;
+                Console.WriteLine($"{q} {r}");
+            }
+        }
+    }
+}
