3d583ef000a9e75236a221da1bc577f113778e6e
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-08-16 19:56:38 -0300

    1848 - Counting Crow

---
+++ VS-files/1848/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Program1848
+    {
+        static void Main(string[] args)
+        {
+            string[] blinkCombinations = { "--*", "-*-", "-**", "*--", "*-*", "**-", "***" };
+
+            for (int i = 0; i < 3; i++)
+            {
+                int result = 0;
+
+                while (true)
+                {
+                    string input = Console.ReadLine();
+                    if (input == "caw caw")
+                        break;
+
+                    result += Array.IndexOf(blinkCombinations, input) + 1;
+                }
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
