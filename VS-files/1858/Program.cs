a5c3e2cd09fc65deea30a56bedb96241a6e9c40b
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-08-18 19:01:18 -0300

    1858 - Theon's Answer

---
+++ VS-files/1858/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1858
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine().Split(' ');
+            int[] values = new int[n];
+
+            for (int i = 0; i < n; i++)
+                values[i] = int.Parse(input[i]);
+            
+            int minIndex = 0;
+            int minValue = values[0];
+
+            for (int i = 1; i < n; i++)
+            {
+                if (values[i] < minValue)
+                {
+                    minValue = values[i];
+                    minIndex = i;
+                }
+            }
+
+            Console.WriteLine(minIndex + 1);
+        }
+    }
+}
