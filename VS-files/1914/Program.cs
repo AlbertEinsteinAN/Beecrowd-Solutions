54afa9b0b0be7cd99f537294ea0640e049f45ecc
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-08-19 07:56:23 -0300

    1914 - Whose Turn Is It?

---
+++ VS-files/1914/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1914
+    {
+        static void Main(string[] args)
+        {
+            int qt = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < qt; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                string[] values = Console.ReadLine().Split(' ');
+                int n = int.Parse(values[0]);
+                int m = int.Parse(values[1]);
+                int sum = n + m;
+
+                if ((sum % 2 == 0 && input[1] == "PAR") || (sum % 2 != 0 && input[1] == "IMPAR"))
+                    Console.WriteLine(input[0]);
+                else
+                    Console.WriteLine(input[2]);
+            }
+        }
+    }
+}
