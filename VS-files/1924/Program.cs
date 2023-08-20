b15a4671d8c0c1ec5435540c9d3601b8d0a52ff0
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-08-20 20:05:19 -0300

    1924 - Vitória and Her Indecision

---
+++ VS-files/1924/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1924
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+                Console.ReadLine();
+            
+            Console.WriteLine("Ciencia da Computacao");
+        }
+    }
+}
