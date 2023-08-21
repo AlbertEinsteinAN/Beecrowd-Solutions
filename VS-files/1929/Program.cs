828bcfd823b1222a920af94385092b40acb20cc5
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-08-21 04:54:22 -0300

    1929 - Triangle

---
+++ VS-files/1929/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Beecrowd
+{
+    class Problem1929
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int[] sides = input.Select(int.Parse).OrderBy(n => n).ToArray();
+            int a = sides[0];
+            int b = sides[1];
+            int c = sides[2];
+            int d = sides[3];
+
+            if (a + b > c || a + b > d || b + c > d)
+                Console.WriteLine('S');
+            else
+                Console.WriteLine('N');
+        }
+    }
+}
