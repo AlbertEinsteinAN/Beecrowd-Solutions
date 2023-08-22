e86b0e612ec47969e6c8051ca0cdf0b6617cf619
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-08-22 20:26:55 -0300

    1930 - Electrical Outlet

---
+++ VS-files/1930/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1930
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int[] stripCounts = Array.ConvertAll(input, int.Parse);
+
+            int totalDevices = 0;
+
+            foreach(int stripCount in stripCounts)
+                totalDevices += stripCount;
+            
+            int maxDevices = totalDevices - 3;
+
+            Console.WriteLine(maxDevices);
+        }
+    }
+}
