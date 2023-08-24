c35c4e96c361c78993f997fd153fa25c378e4124
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-08-24 07:36:44 -0300

    1957 - Converting to Hexadecimal

---
+++ VS-files/1957/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1957
+    {
+        static void Main(string[] args)
+        {
+            int decimalNumber = int.Parse(Console.ReadLine());
+            string hexadecimalNumber = DecimalToHexadecimal(decimalNumber);
+
+            Console.WriteLine(hexadecimalNumber);
+        }
+
+        static string DecimalToHexadecimal(int decimalNumber)
+        {
+            if (decimalNumber == 0)
+                return "0";
+
+            char[] hexDigits = "0123456789ABCDEF".ToCharArray();
+            string hexadecimalNumber = "";
+
+            while (decimalNumber > 0)
+            {
+                int remainder = decimalNumber % 16;
+                hexadecimalNumber = hexDigits[remainder] + hexadecimalNumber;
+                decimalNumber /= 16;
+            }
+
+            return hexadecimalNumber;
+        }
+    }
+}
