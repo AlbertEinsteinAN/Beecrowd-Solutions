bd19d909674701fd4dfd9f9f13c946a15be475e7
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-08-25 07:21:07 -0300

    1959 - Regular Simple Polygons

---
+++ VS-files/1959/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1959
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int N = int.Parse(input[0]);
+            int L = int.Parse(input[1]);
+            long perimeter = (long)N * L;
+
+            Console.WriteLine(perimeter);
+        }
+    }
+}
