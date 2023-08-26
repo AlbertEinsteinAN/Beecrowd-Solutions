f7c4c88e74f0329578b137c7dfba4d1b1debc7bf
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-08-26 06:19:27 -0300

    1960 - Roman Numerals for Page Numbers

---
+++ VS-files/1960/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1960
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            string romanNumeral = RomanConverter.ConvertToRoman(num);
+            Console.WriteLine(romanNumeral);
+        }
+    }
+
+    static class RomanConverter
+    {
+        public static string ConvertToRoman(int num)
+        {
+            string[] romanNumerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+            int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+            string roman = "";
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                while (num >= values[i])
+                {
+                    roman += romanNumerals[i];
+                    num -= values[i];
+                }
+            }
+
+            return roman;
+        }
+    }
+}
