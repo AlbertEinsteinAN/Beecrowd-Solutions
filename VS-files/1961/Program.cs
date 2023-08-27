d0cb8bc95fbce92570dae36deaeb44a144d96ca7
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-08-27 07:44:14 -0300

    1961 - Jumping Frog

---
+++ VS-files/1961/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1961
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int p = int.Parse(input[0]);
+            int n = int.Parse(input[1]);
+            int[] jumps = new int[n];
+            bool win = true;
+
+            string[] values = Console.ReadLine().Split();
+
+            for (int i = 0; i < n; i++)
+                jumps[i] = int.Parse(values[i]);
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (Math.Abs(jumps[i] - jumps[i + 1]) > p)
+                {
+                    win = false;
+                    break;
+                }
+            }
+
+            if (win)
+                Console.WriteLine("YOU WIN");
+            else
+                Console.WriteLine("GAME OVER");
+        }
+    }
+}
