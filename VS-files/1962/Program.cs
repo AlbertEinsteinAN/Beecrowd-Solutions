ad12fb82e6b2a19946158bc4c01b81dbd5d72861
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-08-28 06:16:47 -0300

    1962 - A Long, Long Time Ago

---
+++ VS-files/1962/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1962
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < n; i++)
+            {
+                int t = int.Parse(Console.ReadLine());
+
+                if (t < 2015)
+                    Console.WriteLine($"{2015 - t} D.C.");
+                else if (t == 2015)
+                    Console.WriteLine("1 A.C.");
+                else
+                    Console.WriteLine($"{t - 2014} A.C.");
+            }
+            
+        }
+    }
+}
