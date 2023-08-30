a97d31bb4f51d6b34ec58b59a50ce370349a2a2d
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-08-30 06:28:21 -0300

    1973 - Star Trek

---
+++ VS-files/1963/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1963
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            double a = double.Parse(input[0]);
+            double b = double.Parse(input[1]);
+            double total = (b - a) / a * 100;
+
+            float percentage = (float)total;
+
+            Console.WriteLine($"{percentage:F2}%");
+        }
+    }
+}
