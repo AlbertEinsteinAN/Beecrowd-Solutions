035b175adab8dd8694683b2cded7dccc79a68504
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-08-31 06:59:06 -0300

    1983 - The Chosen

---
+++ VS-files/1983/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1983
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int bestRegistrationNumber = -1;
+            double bestScore = -1.0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                int registrationNumber = int.Parse(input[0]);
+                double score = double.Parse(input[1]);
+
+                if (score >= 8.0 && score > bestScore)
+                {
+                    bestRegistrationNumber = registrationNumber;
+                    bestScore = score;
+                }
+            }
+
+            if (bestRegistrationNumber != -1)
+                Console.WriteLine(bestRegistrationNumber);
+            else
+                Console.WriteLine("Minimum note not reached");
+        }
+    }
+}
