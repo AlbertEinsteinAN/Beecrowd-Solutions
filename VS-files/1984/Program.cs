c2383448aff7bb9201842de7a627c576accad3f1
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-09-02 16:57:59 -0300

    1984 - The Pronalância Puzzle

---
+++ VS-files/1984/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1984
+    {
+        static void Main(string[] args)
+        {
+            char[] num = Console.ReadLine().ToCharArray();
+            int lnth = num.Length;
+            int i, j;
+            char temp;
+
+            for (i = 0, j = lnth - 1; i < lnth / 2; i++, j--)
+            {
+                temp = num[i];
+                num[i] = num[j];
+                num[j] = temp;
+            }
+
+            Console.WriteLine(new string(num));
+        }
+    }
+}
