8e24f7cba9f88443e94a24f8fee5b1c4d50ffccc
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-09-03 08:17:44 -0300

    1985 - MacPRONALTS

---
+++ VS-files/1985/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem1985
+    {
+        static void Main(string[] args)
+        {
+            int p = int.Parse(Console.ReadLine());
+            double[] values = {1.5, 2.5, 3.5, 4.5, 5.5};
+            double sum = 0;
+            
+            for (int i = 0; i < p; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                int input0 = int.Parse(input[0]);
+                double input1 = double.Parse(input[1]);
+                sum += values[input0 - 1001] * input1;
+            }
+            Console.WriteLine(sum.ToString("F2"));
+        }
+    }
+}
