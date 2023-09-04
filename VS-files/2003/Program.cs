acabef4ce7463261080b67953f1bd650fd585676
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-09-04 20:39:14 -0300

    2003 - Sunday Morning

---
+++ VS-files/2003/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem2003
+    {
+        static void Main(string[] args)
+        {
+            string line;
+
+            while ((line = Console.ReadLine()) != null)
+            {
+                string[] time = line.Split(':');
+                int hour = int.Parse(time[0]);
+                int min = int.Parse(time[1]);
+
+                switch (hour)
+                {
+                    case 7:
+                        Console.WriteLine("Atraso maximo: " + min);
+                        break;
+                    case 8:
+                        Console.WriteLine("Atraso maximo: " + (60 + min));
+                        break;
+                    case 9:
+                        Console.WriteLine("Atraso maximo: " + (120 + min));
+                        break;
+                    default:
+                        Console.WriteLine("Atraso maximo: 0");
+                        break;
+                }
+            }
+        }
+    }
+}
