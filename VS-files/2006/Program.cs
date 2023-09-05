6db055eeae0ef79eb1cfaa4cd8cc76537bae5c47
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-09-05 18:06:17 -0300

    2006 - Identifying Tea

---
+++ VS-files/2006/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem2006
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            string[] contestants = Console.ReadLine().Split();
+            int output = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (int.Parse(contestants[i]) == t)
+                    output ++;
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
