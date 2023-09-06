0a34feee5ed8ab3a910d35b0b983230c3736357b
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-09-06 20:44:45 -0300

    2031 - Rock, Paper, Airstrike

---
+++ VS-files/2031/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        string jogador1, jogador2;
+
+        for (int i = 0; i < N; i++)
+        {
+            jogador1 = Console.ReadLine();
+            jogador2 = Console.ReadLine();
+
+            if (string.Equals(jogador1, "ataque", StringComparison.OrdinalIgnoreCase) && string.Equals(jogador2, "ataque", StringComparison.OrdinalIgnoreCase))
+                Console.WriteLine("Aniquilacao mutua");
+            else if (string.Equals(jogador1, "papel", StringComparison.OrdinalIgnoreCase) && string.Equals(jogador2, "papel", StringComparison.OrdinalIgnoreCase))
+                Console.WriteLine("Ambos venceram");
+            else if (string.Equals(jogador1, "ataque", StringComparison.OrdinalIgnoreCase))
+                Console.WriteLine("Jogador 1 venceu");
+            else if (string.Equals(jogador2, "ataque", StringComparison.OrdinalIgnoreCase))
+                Console.WriteLine("Jogador 2 venceu");
+            else if (string.Equals(jogador2, "papel", StringComparison.OrdinalIgnoreCase))
+                Console.WriteLine("Jogador 1 venceu");
+            else if (string.Equals(jogador1, "papel", StringComparison.OrdinalIgnoreCase))
+                Console.WriteLine("Jogador 2 venceu");
+            else
+                Console.WriteLine("Sem ganhador");
+        }
+    }
+}
