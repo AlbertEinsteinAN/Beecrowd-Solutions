a0480a387f96fd99eb6c30d3c8be51191aefe555
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-09-07 07:00:18 -0300

    2057 - Time Zone

---
+++ VS-files/2057/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem2057
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int s = int.Parse(input[0]);
+            int t = int.Parse(input[1]);
+            int f = int.Parse(input[2]);
+            int hour = s + t + f;
+
+            if (hour >= 24)
+                hour -= 24;
+            else if (hour < 0)
+                hour += 24;
+
+            Console.WriteLine(hour);
+        }
+    }
+}
