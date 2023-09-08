62423c74c6ceb15e07f63f8b1889174ebd256282
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-09-08 10:32:25 -0300

    2059 - Odd, Even or Cheating

---
+++ VS-files/2059/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem2059
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int p = int.Parse(input[0]);
+            int j1 = int.Parse(input[1]);
+            int j2 = int.Parse(input[2]);
+            int r = int.Parse(input[3]);
+            int a = int.Parse(input[4]);
+
+            if (r == 1 && a == 1)
+                Console.WriteLine("Jogador 2 ganha!");
+            else if (r == 0 && a == 1)
+                Console.WriteLine("Jogador 1 ganha!");
+            else if (r == 1 && a == 0)
+                Console.WriteLine("Jogador 1 ganha!");
+            else
+            {
+                if ((j1 + j2) % 2 == p)
+                    Console.WriteLine("Jogador 2 ganha!");
+                else
+                    Console.WriteLine("Jogador 1 ganha!");
+            }
+        }
+    }
+}
