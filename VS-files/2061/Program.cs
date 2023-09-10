b9b53a09aa19c7eba2f013419541bebb53d7b651
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-09-10 07:05:33 -0300

    2061 - Closing Tabs

---
+++ VS-files/2061/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem2061
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int n = int.Parse(input[0]);
+            int m = int.Parse(input[1]);
+            int tabs = 0; 
+            string action;
+            
+            for (int i = 0; i < m; i++)
+            {
+                if ((action = Console.ReadLine()) == "fechou")
+                    n++;
+                else
+                    n--;
+            }
+            
+            Console.WriteLine(n);
+        }
+    }
+}
