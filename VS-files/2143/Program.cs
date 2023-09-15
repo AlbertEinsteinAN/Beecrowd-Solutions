3f4fb5461b2d166b5500f3ee88f857c2af8f4317
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-09-15 07:06:15 -0300

    2143 - The Return of Radar

---
+++ VS-files/2143/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem2143
+    {
+        static void Main(string[] args)
+        {
+            int t;
+            
+            while ((t = Convert.ToInt32(Console.ReadLine())) != 0)
+            {
+                for (int i = 0; i < t; i++)
+                {
+                    int n = Convert.ToInt32(Console.ReadLine());
+
+                    if (n % 2 == 1)
+                        Console.WriteLine(n * 2 - 1);
+                    else
+                        Console.WriteLine(n * 2 - 2);
+                }
+            }
+        }
+    }
+}
