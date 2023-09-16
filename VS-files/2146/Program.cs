5fb8895e2249047bc5530db52de4698752832a4c
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-09-16 07:22:55 -0300

    2146 - Password

---
+++ VS-files/2146/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem2146
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            while ((n = Convert.ToInt32(Console.ReadLine())) != 0)
+            {
+                int sum = n - 1;
+                
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
