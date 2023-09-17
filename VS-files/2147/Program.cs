5912f6c6892b1204661c130c7b53e12345ff8e95
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-09-17 05:25:01 -0300

    2147 - Galopeira

---
+++ VS-files/2147/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem2147
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string c = Console.ReadLine();
+                double length = c.Length;
+                string sLength = $"{length / 100:F2}";
+                
+                Console.WriteLine(sLength);
+            }
+        }
+    }
+}
