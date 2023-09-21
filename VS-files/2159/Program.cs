694e18cf7382084a322b6c7b0aad25d32d868ee6
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-09-21 17:57:48 -0300

    2159 - Approximate Number of Primes

---
+++ VS-files/2159/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem2159
+    {
+        static void Main(string[] args)
+        {
+            double n, ans1, ans2;
+            n = double.Parse(Console.ReadLine());
+
+            ans1 = n / Math.Log(n);
+            ans2 = 1.25506 * (n / Math.Log(n));
+
+            Console.WriteLine($"{ans1:F1} {ans2:F1}");
+        }
+    }
+}
