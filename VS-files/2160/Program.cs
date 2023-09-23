85574d6090d3bc9870cab20dd0754f1518987577
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-09-23 14:18:54 -0300

    2160 - Name at Form

---
+++ VS-files/2160/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem2160
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(input.Length > 80 ? "NO" : "YES");
+        }
+    }
+}
