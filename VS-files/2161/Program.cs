892e6afa0a5f3500619a5f947025d701ae420b66
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-09-24 05:15:17 -0300

    2161 - Square Root of 10

---
+++ VS-files/2161/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem2161
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            double ans = 0.0;
+
+            while (n > 0)
+            {
+                ans += 6.0;
+                ans = 1.0 / ans;
+                n--;
+            }
+
+            ans += 3.0;
+            Console.WriteLine(ans.ToString("F10"));
+        }
+    }
+}
