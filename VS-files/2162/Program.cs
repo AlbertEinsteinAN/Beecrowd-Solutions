bdc603b7744d65b32e2444845ccd306df93ed1c0
Albert Einstein Araujo Nogueira <69691876+AlbertEinsteinAN@users.noreply.github.com>
2023-09-25 05:14:38 -0300

    2162 - Peaks and Valleys

---
+++ VS-files/2162/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Problem2162
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int cndtn = 1;
+            int[] ara = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            
+            if (n == 2 && ara[0] == ara[1])
+                cndtn = 0;
+            else
+            {
+                for (int i = 2; i < n; i++)
+                {
+                    if ((ara[i] >= ara[i - 1] && ara[i - 1] >= ara[i - 2]) || (ara[i] <= ara[i - 1] && ara[i - 1] <= ara[i - 2]))
+                    {
+                        cndtn = 0;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine((cndtn == 1) ? "1" : "0");
+        }
+    }
+}
